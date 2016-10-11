f7e48c5667b722c1d012addd7a0ffa1c85baeb18
Joe Buhlig <joe@joebuhlig.com>
2016-10-11 10:24:28 -0500

    Initial commit

---
+++ buffalo-covenant-customizations.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @package BCC
+ */
+/*
+Plugin Name: Buffalo Covenant Customizations
+Plugin URI: https://akismet.com
+Version: 0.1.0
+Author: Joe Buhlig
+Author URI: http://joebuhlig.com
+GitHub Plugin URI: https://github.com/joebuhlig/buffalo-covenant-customizations
+License: GNU General Public License v2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+Text Domain: buffalo-covenant-theme
+*/
+
+?>
