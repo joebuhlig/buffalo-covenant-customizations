6c2263aec67f30b6d3e59f0a93c802551f563ed3
Joe Buhlig <joe@joebuhlig.com>
2016-10-13 07:47:33 -0500

    Added sermon rewrite functions and sidebar shortcode

--- buffalo-covenant-customizations.php
+++ buffalo-covenant-customizations.php
@@ -4,8 +4,8 @@
  */
 /*
 Plugin Name: Buffalo Covenant Customizations
-Plugin URI: https://akismet.com
-Version: 0.1.0
+Plugin URI: https://github.com/joebuhlig/buffalo-covenant-customizations
+Version: 0.1.1
 Author: Joe Buhlig
 Author URI: http://joebuhlig.com
 GitHub Plugin URI: https://github.com/joebuhlig/buffalo-covenant-customizations
@@ -14,4 +14,42 @@ License URI: http://www.gnu.org/licenses/gpl-2.0.html
 Text Domain: buffalo-covenant-theme
 */
 
+
+add_action( 'init', 'wpse26388_rewrites_init' );
+function wpse26388_rewrites_init(){
+    add_rewrite_rule(
+        '^sermons/([a-zA-Z0-9\-]+)/?',
+        'index.php?pagename=sermons&audio_url=$matches[1]',
+        'top' );
+}
+
+add_filter( 'query_vars', 'wpse26388_query_vars' );
+function wpse26388_query_vars( $query_vars ){
+    $query_vars[] = 'audio_url';
+    return $query_vars;
+}
+
+add_filter( 'page_template', 'wpa3396_page_template' );
+function wpa3396_page_template( $page_template )
+{
+    if ( is_page( 'sermons' ) ) {
+        $page_template = dirname( __FILE__ ) . '/page-sermons.php';
+    }
+    return $page_template;
+}
+
+
+function sidebar_shortcode($atts, $content="null"){
+	extract(shortcode_atts(array('name' => ''), $atts));
+	
+	ob_start();
+    dynamic_sidebar($name);
+    $sidebar= ob_get_contents();
+    ob_end_clean();
+	
+	return $sidebar;
+	
+	
+}
+add_shortcode('get_sidebar', 'sidebar_shortcode');
 ?>
